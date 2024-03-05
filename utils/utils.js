423eaf6839be7d2ddc424a0588d8b794615d1fc2
floryn08 <floryn.euro.2007@gmail.com>
2024-03-05 23:25:02 +0200

    chore: util

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { config } = require("../config")
+const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.start = async (interaction) => {
