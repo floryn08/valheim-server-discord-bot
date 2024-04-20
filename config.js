ea41e1ff5c2325a18698c6a5561f2b985ee6bb2d
Florin Vasilica <floryn.euro.2007@gmail.com>
2024-04-20 16:55:44 +0300

    feat: update for k8s deployment (#18)

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 
 exports.config = {
-    "token": process.env.DISCORD_TOKEN,
-    "clientId": process.env.DISCORD_CLIENT_ID,
-    "guildId": process.env.GUILD_ID,
-    "portainerEndpoint": process.env.PORTAINER_URL,
-    "portainerApiKey": process.env.PORTAINER_API_KEY,
-    "containerName": process.env.CONTAINER_NAME,
-    "serverName": process.env.SERVER_NAME,
-    "joinCodeLoopCount": process.env.JOIN_CODE_LOOP_COUNT,
-    "joinCodeLoopTimeoutMillis": process.env.JOIN_CODE_LOOP_TIMEOUT_MILLIS
-}
+  token: process.env.DISCORD_TOKEN,
+  clientId: process.env.DISCORD_CLIENT_ID,
+  guildId: process.env.GUILD_ID,
+  deploymentName: process.env.DEPLOYMENT_NAME,
+  namespace: process.env.NAMESPACE,
+  serverName: process.env.SERVER_NAME,
+  joinCodeLoopCount: process.env.JOIN_CODE_LOOP_COUNT,
+  joinCodeLoopTimeoutMillis: process.env.JOIN_CODE_LOOP_TIMEOUT_MILLIS,
+};
--- utils/utils.js
+++ utils/utils.js
@@ -1,131 +1,67 @@
-const axios = require('axios');
-const { config } = require("../config")
+const { config } = require("../config");
+const k8s = require("@kubernetes/client-node");
+
+// Define the deployment name and namespace
+const deploymentName = config.deploymentName;
+const namespace = config.namespace;
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
 
 exports.start = async (interaction) => {
-    await interaction.reply('Starting server...')
-    console.log('Starting server...')
+  await interaction.reply("Starting server...");
+  console.log("Starting server...");
 
-    if (!config.portainerEndpoint || !config.portainerApiKey) {
-        throw new Error('Please make sure you have set up the Portainer API token in your code.');
-    }
+  // find the particular deployment
+  const res = await k8sApi.readNamespacedDeployment(deploymentName, namespace);
+  let deployment = res.body;
 
-    await axios({
-        url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/start`,
-        method: 'POST',
-        headers: {
-            "X-API-Key": `${config.portainerApiKey}`
-        }
-    }).then(async function (startResponse) {
+  // edit
+  deployment.spec.replicas = 1;
 
-        // wait after starting the container because it may have some old logs 
-        // and the old join code may be returned, so we wait a bit for the new container 
-        // to log some new lines and then start the check loop
-        await new Promise(resolve => setTimeout(resolve, config.joinCodeLoopTimeoutMillis));
+  // replace
+  await k8sApi.replaceNamespacedDeployment(
+    deploymentName,
+    namespace,
+    deployment
+  );
 
-        for (let i = 0; i < config.joinCodeLoopCount; i++) {
-
-            let joinCode = "";
-
-            await axios({
-                url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/logs`,
-                method: 'GET',
-                params: {
-                    stdout: true,
-                    tail: 10 // last 10 lines of the log
-                },
-                headers: {
-                    "X-API-Key": `${config.portainerApiKey}`
-                }
-            }).then(function (logsResponse) {
-                let data = logsResponse.data.toString();
-                // Check if the data contains the string
-                let index = data.indexOf(`Session "${config.serverName}" with join code`);
-                if (index !== -1) {
-                    // Get the next word after the string
-                    let words = data.slice(index).split(' ');
-                    joinCode = words[5]; // The next word is at index 5
-                }
-
-                console.log("Join code not present yet, retryin...");
-            });
-
-            if (startResponse.status == 204 && joinCode != "") {
-                await interaction.followUp(`Server started successfully! Join code is ${joinCode}`)
-                console.log('Server started successfully! Join code is', joinCode);
-
-                break;
-            }
-
-            await new Promise(resolve => setTimeout(resolve, config.joinCodeLoopTimeoutMillis));
-        }
-
-    }).catch(async function (error) {
-
-        if (error.response.status == 304) {
-            await interaction.followUp('Server already started!')
-            console.log('Server already started!');
-        } else {
-            await interaction.followUp(`Failed to start container. Error: ${error}`)
-            console.log(`Failed to start container. Error: ${error}`)
-        }
-
-    });
-
-}
+  await interaction.followUp("Server is running!");
+};
 
 exports.stop = async (interaction) => {
-    await interaction.reply('Stopping server...')
-    console.log('Stopping server...')
+  await interaction.reply("Stopping server...");
+  console.log("Stopping server...");
 
-    if (!config.portainerEndpoint || !config.portainerApiKey) {
-        throw new Error('Please make sure you have set up the Portainer API token in your code.');
-    }
+  // find the particular deployment
+  const res = await k8sApi.readNamespacedDeployment(deploymentName, namespace);
+  let deployment = res.body;
 
-    await axios({
-        url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/stop`,
-        method: 'POST',
-        headers: {
-            "X-API-Key": `${config.portainerApiKey}`
-        }
-    }).then(async function (response) {
+  // edit
+  deployment.spec.replicas = 0;
 
-        if (response.status == 204) {
-            await interaction.followUp('Server stopped successfully!')
-            console.log('Server stopped successfully!');
-        } else {
-            console.log(response.status)
-        }
+  // replace
+  await k8sApi.replaceNamespacedDeployment(
+    deploymentName,
+    namespace,
+    deployment
+  );
 
-    }).catch(async function (error) {
-        if (error.response.status == 304) {
-            await interaction.followUp('Server already stopped!')
-            console.log('Server already stopped!');
-        } else {
-            await interaction.followUp(`Failed to stop server. Error: ${error}`)
-            console.log(`Failed to stop server. Error: ${error}`)
-        }
-    });
-}
+  await interaction.followUp("Server is stopped!");
+
+};
 
 exports.status = async (interaction) => {
-    await interaction.reply('Getting server status...')
-    console.log('Getting server status...');
+  await interaction.reply("Getting server status...");
+  console.log("Getting server status...");
 
-    if (!config.portainerEndpoint || !config.portainerApiKey) {
-        throw new Error('Please make sure you have set up the Portainer API token in your code.');
-    }
+  // find the particular deployment
+  const res = await k8sApi.readNamespacedDeployment(deploymentName, namespace);
 
-    await axios({
-        url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/json`,
-        method: 'GET',
-        headers: {
-            "X-API-Key": `${config.portainerApiKey}`
-        }
-    }).then(async function (response) {
-        await interaction.followUp(`Server status is: ${response.data.State.Status}`)
-        console.log(`Server status is: ${response.data.State.Status}`);
-    }).catch(async function (error) {
-        await interaction.followUp(`Failed to get server status. Error Code: ${error}`)
-        throw new Error(`Failed to get server status. Error Code: ${error}`);
-    });
-}
+  if (res.body.spec.replicas == 0) {
+    await interaction.followUp("Server is stopped!");
+  } else {
+    await interaction.followUp("Server is running!");
+  }
+};
