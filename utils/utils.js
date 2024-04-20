105afe76bfc1f254d75e63a2b87b84e406e09746
Flow <floryn.euro.2007@gmail.com>
2024-04-20 11:57:13 +0300

    chore: update

--- utils/utils.js
+++ utils/utils.js
@@ -1,134 +1,176 @@
-const axios = require('axios');
-const { config } = require("../config")
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const axios = require("axios");
+const { config } = require("../config");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const { KubeClient } = require("@kubernetes/client-node");
+const fs = require("fs");
+
+const kubeconfig = {
+  auth: {
+    username: "valheim",
+    password: fs.readFileSync(
+      "/var/run/secrets/kubernetes.io/serviceaccount/token"
+    ),
+  },
+  server: "https://kubernetes.default.svc",
+};
+
+// Create a KubeClient instance
+const client = new KubeClient(kubeconfig);
+
+// Define the deployment name and namespace
+const deploymentName = 'valheim';
+const namespace = 'game-servers';
 
 exports.start = async (interaction) => {
-    await interaction.reply('Starting server...')
-    console.log('Starting server...')
+  await interaction.reply("Starting server...");
+  console.log("Starting server...");
 
-    if (!config.portainerEndpoint || !config.portainerApiKey) {
-        throw new Error('Please make sure you have set up the Portainer API token in your code.');
-    }
+  if (!config.portainerEndpoint || !config.portainerApiKey) {
+    throw new Error(
+      "Please make sure you have set up the Portainer API token in your code."
+    );
+  }
 
-    await axios({
-        url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/start`,
-        method: 'POST',
-        headers: {
-            "X-API-Key": `${config.portainerApiKey}`
-        }
-    }).then(async function (startResponse) {
+  await axios({
+    url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/start`,
+    method: "POST",
+    headers: {
+      "X-API-Key": `${config.portainerApiKey}`,
+    },
+  })
+    .then(async function (startResponse) {
+      // wait after starting the container because it may have some old logs
+      // and the old join code may be returned, so we wait a bit for the new container
+      // to log some new lines and then start the check loop
+      await new Promise((resolve) =>
+        setTimeout(resolve, config.joinCodeLoopTimeoutMillis)
+      );
 
-        // wait after starting the container because it may have some old logs 
-        // and the old join code may be returned, so we wait a bit for the new container 
-        // to log some new lines and then start the check loop
-        await new Promise(resolve => setTimeout(resolve, config.joinCodeLoopTimeoutMillis));
+      for (let i = 0; i < config.joinCodeLoopCount; i++) {
+        let joinCode = "";
 
-        for (let i = 0; i < config.joinCodeLoopCount; i++) {
+        await axios({
+          url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/logs`,
+          method: "GET",
+          params: {
+            stdout: true,
+            tail: 10, // last 10 lines of the log
+          },
+          headers: {
+            "X-API-Key": `${config.portainerApiKey}`,
+          },
+        }).then(function (logsResponse) {
+          let data = logsResponse.data.toString();
+          // Check if the data contains the string
+          let index = data.indexOf(
+            `Session "${config.serverName}" with join code`
+          );
+          if (index !== -1) {
+            // Get the next word after the string
+            let words = data.slice(index).split(" ");
+            joinCode = words[5]; // The next word is at index 5
+          }
 
-            let joinCode = "";
+          console.log("Join code not present yet, retryin...");
+        });
 
-            await axios({
-                url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/logs`,
-                method: 'GET',
-                params: {
-                    stdout: true,
-                    tail: 10 // last 10 lines of the log
-                },
-                headers: {
-                    "X-API-Key": `${config.portainerApiKey}`
-                }
-            }).then(function (logsResponse) {
-                let data = logsResponse.data.toString();
-                // Check if the data contains the string
-                let index = data.indexOf(`Session "${config.serverName}" with join code`);
-                if (index !== -1) {
-                    // Get the next word after the string
-                    let words = data.slice(index).split(' ');
-                    joinCode = words[5]; // The next word is at index 5
-                }
+        if (startResponse.status == 204 && joinCode != "") {
+          await interaction.followUp(
+            `Server started successfully! Join code is ${joinCode}`
+          );
+          console.log("Server started successfully! Join code is", joinCode);
 
-                console.log("Join code not present yet, retryin...");
-            });
-
-            if (startResponse.status == 204 && joinCode != "") {
-                await interaction.followUp(`Server started successfully! Join code is ${joinCode}`)
-                console.log('Server started successfully! Join code is', joinCode);
-
-                break;
-            }
-
-            await new Promise(resolve => setTimeout(resolve, config.joinCodeLoopTimeoutMillis));
-        }
-
-    }).catch(async function (error) {
-
-        if (error.response.status == 304) {
-            await interaction.followUp('Server already started!')
-            console.log('Server already started!');
-        } else {
-            await interaction.followUp(`Failed to start container. Error: ${error}`)
-            console.log(`Failed to start container. Error: ${error}`)
+          break;
         }
 
+        await new Promise((resolve) =>
+          setTimeout(resolve, config.joinCodeLoopTimeoutMillis)
+        );
+      }
+    })
+    .catch(async function (error) {
+      if (error.response.status == 304) {
+        await interaction.followUp("Server already started!");
+        console.log("Server already started!");
+      } else {
+        await interaction.followUp(
+          `Failed to start container. Error: ${error}`
+        );
+        console.log(`Failed to start container. Error: ${error}`);
+      }
     });
-
-}
+};
 
 exports.stop = async (interaction) => {
-    await interaction.reply('Stopping server...')
-    console.log('Stopping server...')
+  await interaction.reply("Stopping server...");
+  console.log("Stopping server...");
 
-    if (!config.portainerEndpoint || !config.portainerApiKey) {
-        throw new Error('Please make sure you have set up the Portainer API token in your code.');
-    }
+  if (!config.portainerEndpoint || !config.portainerApiKey) {
+    throw new Error(
+      "Please make sure you have set up the Portainer API token in your code."
+    );
+  }
 
-    await axios({
-        url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/stop`,
-        method: 'POST',
-        headers: {
-            "X-API-Key": `${config.portainerApiKey}`
-        }
-    }).then(async function (response) {
-
-        if (response.status == 204) {
-            await interaction.followUp('Server stopped successfully!')
-            console.log('Server stopped successfully!');
-        } else {
-            console.log(response.status)
-        }
-
-    }).catch(async function (error) {
-        if (error.response.status == 304) {
-            await interaction.followUp('Server already stopped!')
-            console.log('Server already stopped!');
-        } else {
-            await interaction.followUp(`Failed to stop server. Error: ${error}`)
-            console.log(`Failed to stop server. Error: ${error}`)
-        }
+  await axios({
+    url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/stop`,
+    method: "POST",
+    headers: {
+      "X-API-Key": `${config.portainerApiKey}`,
+    },
+  })
+    .then(async function (response) {
+      if (response.status == 204) {
+        await interaction.followUp("Server stopped successfully!");
+        console.log("Server stopped successfully!");
+      } else {
+        console.log(response.status);
+      }
+    })
+    .catch(async function (error) {
+      if (error.response.status == 304) {
+        await interaction.followUp("Server already stopped!");
+        console.log("Server already stopped!");
+      } else {
+        await interaction.followUp(`Failed to stop server. Error: ${error}`);
+        console.log(`Failed to stop server. Error: ${error}`);
+      }
     });
-}
+};
 
 exports.status = async (interaction) => {
-    await interaction.reply('Getting server status...')
-    console.log('Getting server status...');
+  await interaction.reply("Getting server status...");
+  console.log("Getting server status...");
 
-    const { stdout, stderr } = await exec(`argocd login 192.168.0.150:8090 --username admin --password ${config.argoPass}`);
+  // Get the current replicas for the deployment
+  await client.apis/apps.v1.namespaces[namespace].deployments.get(deploymentName).then((deployment) => {
+    const currentReplicas = deployment.spec.replicas;
+    console.log(`Current replicas: ${currentReplicas}`);
 
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-
-    // await axios({
-    //     url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/json`,
-    //     method: 'GET',
-    //     headers: {
-    //         "X-API-Key": `${config.portainerApiKey}`
-    //     }
-    // }).then(async function (response) {
-    //     await interaction.followUp(`Server status is: ${response.data.State.Status}`)
-    //     console.log(`Server status is: ${response.data.State.Status}`);
-    // }).catch(async function (error) {
-    //     await interaction.followUp(`Failed to get server status. Error Code: ${error}`)
-    //     throw new Error(`Failed to get server status. Error Code: ${error}`);
+    // // Decrease the replicas to 0
+    // client.apis.apps.v1.namespaces[namespace].deployments.patch(deploymentName, {
+    //   spec: { replicas: 0 },
+    // }).then((response) => {
+    //   console.log('Successfully patched deployment');
+    //   fs.writeFileSync('deployment-patched.json', JSON.stringify(response));
+    // }).catch((error) => {
+    //   console.error(error);
     // });
-}
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  // await axios({
+  //     url: `${config.portainerEndpoint}/api/endpoints/2/docker/containers/${config.containerName}/json`,
+  //     method: 'GET',
+  //     headers: {
+  //         "X-API-Key": `${config.portainerApiKey}`
+  //     }
+  // }).then(async function (response) {
+  //     await interaction.followUp(`Server status is: ${response.data.State.Status}`)
+  //     console.log(`Server status is: ${response.data.State.Status}`);
+  // }).catch(async function (error) {
+  //     await interaction.followUp(`Failed to get server status. Error Code: ${error}`)
+  //     throw new Error(`Failed to get server status. Error Code: ${error}`);
+  // });
+};
