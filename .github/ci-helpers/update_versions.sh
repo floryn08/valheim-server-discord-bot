20d59abefbf29308d0cb988cb050ada9aa25a85b
Florin Vasilica <floryn.euro.2007@gmail.com>
2025-05-13 15:27:36 +0300

    fix: update discordBotImage version substitution in update_versions.sh

--- .github/ci-helpers/update_versions.sh
+++ .github/ci-helpers/update_versions.sh
@@ -11,4 +11,4 @@ version=$1
 
 sed -i "s/^version:.*/version: ${version}/" deployment/helm/Chart.yaml
 sed -i "s/^appVersion:.*/appVersion: ${version}/" deployment/helm/Chart.yaml
-sed -i "s/:[0-9]\+\.[0-9]\+\.[0-9]\+/:${version}/" deployment/helm/values.yaml
+sed -i "s|\(discordBotImage: .*:\)[0-9]\+\.[0-9]\+\.[0-9]\+|\1${version}|" deployment/helm/values.yaml
