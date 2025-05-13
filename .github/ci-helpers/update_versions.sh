728674aa263a4b3df10c4449ff5a8cc4f5fc8cad
Florin Vasilica <floryn.euro.2007@gmail.com>
2025-05-13 15:40:16 +0300

    fix: enhance version update script with before/after output

--- .github/ci-helpers/update_versions.sh
+++ .github/ci-helpers/update_versions.sh
@@ -9,6 +9,19 @@ fi
 # Assign the parameter to a variable
 version=$1
 
+echo "Setting version to $version..."
+
+# Print the original lines
+echo "Before:"
+grep "discordBotImage:" deployment/helm/values.yaml
+
+# Update Chart.yaml
 sed -i "s/^version:.*/version: ${version}/" deployment/helm/Chart.yaml
 sed -i "s/^appVersion:.*/appVersion: ${version}/" deployment/helm/Chart.yaml
-sed -i "s|valheim-server-discord-bot:[^[:space:]]*|valheim-server-discord-bot:${version}|" deployment/helm/values.yaml
+
+# Update image tag in values.yaml
+sed -i "s|\(discordBotImage: .*:\)[0-9]\+\.[0-9]\+\.[0-9]\+|\1${version}|" deployment/helm/values.yaml
+
+# Print the result
+echo "After:"
+grep "discordBotImage:" deployment/helm/values.yaml
